6de538470ad26e5b9060448c9b33ed606c99041f
Chris H <chrissy14az@hotmail.co.uk>
2019-11-07 12:10:42 +0000

    Finished Activity 3

---
+++ src/objectinstances/FirstInstance.java
@@ -0,0 +1,24 @@
+package objectinstances;
+
+public class FirstInstance
+{
+
+    public static void main(String[] args)
+    {
+
+        // Fetch car information and call acceleration method (from 'Car' object class)
+        System.out.println("PAINT STYLE:\t" + Car.colour);
+        System.out.println("BODY TYPE:\t\t" + Car.bodyType);
+        System.out.println(Car.accelerate());
+
+        // Create new instance of 'Car' class (an object)
+        Car BMW = new Car();
+
+        // Now fetch the information as above for the new instance
+        System.out.println("\nBMW:");
+        System.out.println("PAINT STYLE:\t" + BMW.colour);
+        System.out.println("BODY TYPE:\t\t" + BMW.bodyType);
+
+    }
+
+}
